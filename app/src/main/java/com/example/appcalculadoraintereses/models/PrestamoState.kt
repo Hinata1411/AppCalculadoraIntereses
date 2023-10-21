4eccdc5567f0f6e439e15554054e1e5838bf9b01
Marta <85373964+Hinata1411@users.noreply.github.com>
2023-10-21 07:44:11 -0600

    Nueva rama de la app

---
+++ app/src/main/java/com/example/appcalculadoraintereses/models/PrestamoState.kt
@@ -0,0 +1,11 @@
+package com.example.appcalculadoraintereses.models
+
+data class PrestamoState(
+    val montoPrestamo: String = "",
+    val cantCuotas: String = "",
+    val tasa: String = "",
+    val montoIntereses: Double = 0.0,
+    val montoCuota: Double = 0.0,
+    var showAlert: Boolean = false
+
+)
